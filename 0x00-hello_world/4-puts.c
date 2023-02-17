15c00ee856e902b09129b63668743934e84b9c73
Efeturi Akomire <learning.ameo@gmail.com>
2023-02-17 12:55:31 -0800

    C-0x00 Hello World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,2 @@
 #!/bin/bash
-int main() { char usingPuts [] = "\"Programming is like building a multilingual puzzle" puts(usingPuts); return 0; }
+int	main()	{	char str[]	=	"\"Programming is like building a multilingual puzzle\n";	char	answer[]	=	puts(str);	gcc	-o	number4	$answer	-lm	return	0;	}
