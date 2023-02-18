abe94ef9c727d011c711e0f2507f70d7ba5199fa
Efeturi Akomire <learning.ameo@gmail.com>
2023-02-18 13:50:55 -0800

    C-0x00 Hello World

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 /**
- * main - This program uses printf to print a line.
- * return: Program suceeds when it returns 0
- * usingPrint is the string for the printf function
- * %s\n tells printsf to expect a string and add a new line after it
+ * main - this program uses printf to print a line
+ * return: (0) return value when successful
 */
-int main()
+
+int main(void)
 {
 	char usingPrint[] = "with proper grammar, but the outcome is a piece of art,";
+
 	printf("%s\n", usingPrint);
-	return 0;
+
+	return (0);
 }
