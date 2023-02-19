c5e0c85b2e06c59e09436b10b2fc441383b765b5
Efeturi Akomire <learning.ameo@gmail.com>
2023-02-19 06:49:52 -0800

    C-0x00 Hello World

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 /**
- * main - This is a program to print out the size of all the major variables in c
+ * main - This is a program to print out the
+ * size of all the major variables in c
  * Return: 0 Return gives Zero (0) when the program is uccessful
  * Using printf to get the output to standard diplay
  * defining the variable but not giving it a value means it interpretes itself
@@ -16,11 +17,11 @@ int main(void)
 	long long int thisLLI;
 	float thisFloat;
 
-	printf("Size of a char: %zu bytes\n", sizeof(thisChar));
-	printf("Size of an int: %zu bytes\n", sizeof(thisInt));
-	printf("Size of a long int: %zu bytes\n", sizeof(thisLI));
-	printf("Size of a long long int: %zu bytes\n", sizeof(thisLLI));
-	printf("Size of a float: %zu bytes\n", sizeof(thisFloat));
+	printf("Size of a char: %zu byte(s)\n", sizeof(thisChar));
+	printf("Size of an int: %zu byte(s)\n", sizeof(thisInt));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(thisLI));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(thisLLI));
+	printf("Size of a float: %zu byte(s)\n", sizeof(thisFloat));
 
 return (0);
 }
