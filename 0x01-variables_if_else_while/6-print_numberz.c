9c18db8076c39a57514e2ec1062b916ab8f17d69
Efeturi Akomire <learning.ameo@gmail.com>
2023-02-20 08:42:11 -0800

    6. Numbers using while and ASCII

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -6,13 +6,14 @@
  * no two digit numbers
  * while is part of the stdio library
  * Return: 0 when successful
+ * while uses ASCII and in that case 0 is 48 and 10 is 58
 */
 
 int main(void)
 {
-	int i = 0;
+	int i = 48;
 
-	while (i < 10)
+	while (i < 58)
 	{
 		putchar(i);
 		i++;
