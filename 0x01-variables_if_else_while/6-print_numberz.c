8df6b2d217fedc457ed1b987885c9b22400f2ae1
Efeturi Akomire <learning.ameo@gmail.com>
2023-02-20 08:21:37 -0800

    6. A program to print positive single digit numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - This program outputs numbers from 0-10 without
+ * using prints or puts
+ * i to indicate the number and conditions of i<10 to show
+ * no two digit numbers
+ * Return: 0 when successful
+*/
+int main(void)
+{
+	int i = 10;
+
+	while (i < 10)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
