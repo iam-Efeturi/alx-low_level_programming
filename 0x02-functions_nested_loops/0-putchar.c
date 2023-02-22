60881adabbc47f449b720f4c55ef6eca5f8fbc30
Efeturi Akomire <learning.ameo@gmail.com>
2023-02-22 11:56:00 -0800

    0. Putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/* main - A program to return a phrase
+ * Return: 0 Program returns 0 when successful
+ * Using write to print the _putchar phrase
+*/
+
+int main(void)
+
+{
+       write(1, "_putchar\n", 8);
+
+return(0)       
+}
