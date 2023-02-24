9aeb6b15363c44e62135a33fe95500617bdb87b8
Efeturi Akomire <learning.ameo@gmail.com>
2023-02-24 09:09:50 -0800

    0x02 nestled loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - This program returns prints a-z 10x
+ * Return: 0 Program returns zero when successful
+ * Gonna be a nested loop
+*/
+
+void print_alphabet_x10(void)
+
+{
+	int m = 0;
+
+	while
+		(m < 10);
+
+	{
+		char letters = a;
+
+		for (char letters = a; letters <= z; letters++)
+			_putchar('letter')
+	}
+
+	_putchar("\n");
+	m++;
+
+	return (0);
+}
