367c85d6077f52e955bbdd142289796874b0958d
Efeturi Akomire <learning.ameo@gmail.com>
2023-02-24 06:30:35 -0800

    0. putchar and main files

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,31 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+/* This is the library file for the new project
+ * no use of the regular file for this project
+ * we use #ifndef to make MAIN_H_ the library
+ * then #define MAIN_H_ to define all the terms
+ * all stated below are now functions we can use.
+ * _putchar(char c) means it'll return the char c
+*/
+
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int islower(int c);
+int _islower(int c);
+int isalpha(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
+
+
+
+
+#endif
