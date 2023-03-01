f4412e94a28a32c8e0c069912e30f33f86b87344
Efeturi Akomire <learning.ameo@gmail.com>
2023-03-01 07:40:01 -0800

    0x02 nestled loops

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - This program checks if the input is lowercase
+ * Return: 0
+ * using char to signify the character
+ * @j; j is the character we want to check
+ * Return: 1 if lowercase, meaning yes, 0 if otherwise
+*/
+
+int _islower(int j)
+{
+	return (j >= 'a' && c <= 'z');
+}
