151c09f4868b06f04ac1735f71dc045cc2aff61d
Efeturi Akomire <learning.ameo@gmail.com>
2023-03-05 00:11:39 -0800

    0x02 nestled loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs - Program gives the abbsolute value of input
+ * Return: 0 returns the absolute value when successful
+ * using _abs and _putchar
+ * @k: The desired input number
+ * @r: the result
+*/
+
+int _abs(int k, r)
+
+{
+
+r = _abs(k);
+_putchar(r);
+
+return (r);
+
+}
