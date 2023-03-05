81ecca7501360d5f6db656ebc9b001d8084e5cf7
Efeturi Akomire <learning.ameo@gmail.com>
2023-03-05 00:39:30 -0800

    0x02 nestled loops

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last didgit of given number
+ * Return: 0
+ * Will divide the number by 10 and print the remainder
+ * thinking return will be the above
+ * @m: m is the number we're going to divide
+*/
+
+int print_last_digit(int m)
+
+{
+return ((m % 10));
+}
