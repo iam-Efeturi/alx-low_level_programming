15ce65d80872d6746f8f68f2bbf99e05a943bac5
Efeturi Akomire <learning.ameo@gmail.com>
2023-03-04 23:45:37 -0800

    0x02 nestled loops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,13 +2,9 @@
 
 /**
  * print_sign - This program checks if the a number is positive or negative
- *
  * Return: 1 if +ve, 0 if 0, -1 if negative
- *
  * @n: n will be the desired number
- *
  * there will be return statements for each condition
- *
  * using an if statement for this
 */
 
