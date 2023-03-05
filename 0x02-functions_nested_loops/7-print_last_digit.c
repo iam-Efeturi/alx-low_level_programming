d32dcb607ab85c60a9a7cb097aa4efb6e8a90cae
Efeturi Akomire <learning.ameo@gmail.com>
2023-03-05 03:56:13 -0800

    0x02 nestled loops

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -4,12 +4,27 @@
  * print_last_digit - prints the last didgit of given number
  * Return: 0
  * Will divide the number by 10 and print the remainder
- * thinking return will be the above
+ * thinking return will be the above, the remainder.
  * @m: m is the number we're going to divide
 */
 
 int print_last_digit(int m)
 
 {
-return ((m % 10));
+int digit;
+
+if
+	(m < 0)
+	{
+	digit = ((m * (-1)) % 10);
+	_putchar (digit + '0');
+	return (digit);
+	}
+
+else
+	{
+	digit = (m % 10);
+	_putchar (digit + '0');
+	return (digit);
+	}
 }
