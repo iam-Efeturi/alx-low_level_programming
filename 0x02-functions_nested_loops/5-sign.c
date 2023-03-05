f285bcd5eab2404f0804ad06d8a4a3003ca49a89
Efeturi Akomire <learning.ameo@gmail.com>
2023-03-04 23:10:38 -0800

    0x02 nestled loops

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_sign - This program checks if the a number is positive or negative
+ * Return: 1 if +ve, 0 if 0, -1 if negative
+ * @c: c will be the desired number
+ * there will be return statements for each condition
+ * using if too
+*/
+
+int print_sign(int n);
+
+{
+
+if (n > 0)
+	{
+	_putchar('+');
+	return (1);
+	}
+
+else if (n == 0)
+	{
+	_putchar('0');
+	return (0);
+	}
+
+else if (n < 0)
+	{
+	_putchar('-');
+	return (-1);
+	}
+}
