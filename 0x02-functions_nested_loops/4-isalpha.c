780042d495b887c4aac9477d70f50816568add35
Efeturi Akomire <learning.ameo@gmail.com>
2023-03-04 22:24:03 -0800

    0x02 nestled loops

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,10 +1,11 @@
 #include "main.h"
 /**
- * main - Program checks if character is an alphabet
+ * _isalpha - Program checks if character is an alphabet
  * Return: returns 1 if an alphabet or 0 if otherwise
  * @c: c is what we're checking if an alphabet
 */
 
 int _isalpha(int c);
 {
-	return (c >= 'a' && c <= 'z' && c
+	return (c >= 'a' && c <= 'z' && c >= 'A' && c <= 'Z');
+}
