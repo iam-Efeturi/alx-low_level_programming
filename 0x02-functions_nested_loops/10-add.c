9920edb0a494ed06b8b75683f5c4b2ff2cb8e0ad
Efeturi Akomire <learning.ameo@gmail.com>
2023-03-05 21:58:05 -0800

    0x02 nestled loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - Function returns sum of two numbers
+ * Return: 0
+ * @m: our first input m
+ * @n: our secont input n
+*/
+
+int add(int i, int k)
+
+{
+return (i + k);
+}
